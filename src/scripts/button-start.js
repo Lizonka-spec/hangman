e3c8540c5914ee1e4c730151243a56740059a5c7
Lizonka-spec <proudoffyou01@gmail.com>
2025-03-29 14:30:23 +0300

    feat: start button

---
+++ src/scripts/button-start.js
@@ -0,0 +1,6 @@
+let button_start = document.querySelector('.Start_But')
+console.log(button_start);
+button_start.addEventListener('click', function(){
+    // window.open('C:/Users/vladm/OneDrive/Desktop/Frontend/test/game/index.html')
+    window.location.assign('C:/Users/vladm/OneDrive/Desktop/Frontend/hangman/src/game/index.html')
+})
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import '../styles/style.css'
+import './button-start'
 
 document.querySelector('#app').innerHTML = `
   <div>
