53b38ab4dc2196240814d23d603ea5829d9a464e
Lizonka-spec <proudoffyou01@gmail.com>
2025-03-29 16:14:34 +0300

    change new files

--- src/scripts/button-start.js
+++
@@ -1,6 +0,0 @@
-let button_start = document.querySelector('.Start_But')
-console.log(button_start);
-button_start.addEventListener('click', function(){
-    // window.open('C:/Users/vladm/OneDrive/Desktop/Frontend/test/game/index.html')
-    window.location.assign('C:/Users/vladm/OneDrive/Desktop/Frontend/hangman/src/game/index.html')
-})
