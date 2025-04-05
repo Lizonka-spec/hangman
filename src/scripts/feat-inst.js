7355aeb1bb31fbf2cfbc0608e905c5b16167f952
Radok-ninja <radok.zubets@gmail.com>
2025-04-05 13:29:58 +0300

    inst-accodion

---
+++ src/scripts/feat-inst.js
@@ -0,0 +1,18 @@
+
+
+const toggleButton = document.querySelector('.fa-brands.fa-instagram');
+const nicknameList = document.getElementById('nicknameList');
+
+
+toggleButton.addEventListener('click', function() {
+
+  if (nicknameList.style.display === 'none') {
+    nicknameList.style.display = 'block';
+  } else {
+
+    nicknameList.style.display = 'none';
+  }
+});
+
+nicknameList.style.display = 'none';
+
