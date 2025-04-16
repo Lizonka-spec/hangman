0f87051a9786def526d12bb4b68a5d9f48923585
Lizonka-spec <proudoffyou01@gmail.com>
2025-04-16 20:13:35 +0300

    add styles for main page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 import '../styles/style.css';
 import '../styles/button-start.css'
-import { featinst } from './feat-inst'
+
+import { createModal } from "../templates/createModal";
+import { toggleModalRules } from "./toggleModalRules";
+import { showInstList } from './showInstList';
+
+const toggleModalButton = document.querySelector('.button1');
+const modalContainer = document.getElementById("modal");
+modalContainer.innerHTML = createModal();
+const toggleButton = document.querySelector('.fa-brands.fa-instagram');
+toggleButton.addEventListener("click", showInstList);
+
+toggleModalButton.addEventListener("click", toggleModalRules)
