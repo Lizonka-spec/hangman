cf2cf7d130864aad0630fd3e7bf630d5438c2607
Lizonka-spec <proudoffyou01@gmail.com>
2025-04-19 16:38:03 +0300

    feat:GeneratorButton

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,13 +2,16 @@ import '../styles/style.css';
 import '../styles/button-start.css'
 
 import { createModal } from "../templates/createModal";
-import { toggleModalRules } from "./toggleModalRules";
-import { showInstList } from './showInstList';
+import { toggleModalRules } from "../scripts/toggleModalRules";
+import { showInstList } from '../scripts/showInstList';
 
 const toggleModalButton = document.querySelector('.button1');
 const modalContainer = document.getElementById("modal");
 modalContainer.innerHTML = createModal();
 const toggleButton = document.querySelector('.fa-brands.fa-instagram');
 toggleButton.addEventListener("click", showInstList);
+
+
+
 
 toggleModalButton.addEventListener("click", toggleModalRules)
---
+++ src/scripts/getRandomWord.js
@@ -0,0 +1,14 @@
+export async function getRandomWord() {
+        try {
+            const response = await fetch('https://random-word-api.herokuapp.com/word');
+            
+            if(!response.ok){
+                throw new Error("we couldn't find the word");
+            }
+            
+            const data = await response.json();
+            return data[0];
+        } catch (error) {
+            console.log(error.message);   
+        }
+}
