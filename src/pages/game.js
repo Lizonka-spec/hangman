716429d840ec2e40ce494c3495cae6753b031525
Lizonka-spec <proudoffyou01@gmail.com>
2025-04-23 20:54:24 +0300

    feat:GetRandomWord

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,7 +1,7 @@
 import { getRandomWord } from '../scripts/getRandomWord';
 
 const generatorButton = document.querySelector(".generator");
-const wordElement = document.querySelector(".word");
+const wordElement = document.querySelector(".wordElement");
 
 generatorButton.addEventListener("click", async function() {
     const generatedWord = await getRandomWord();
