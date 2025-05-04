9066a5e90d13eb908ff9751399a66c929512edc5
Radok-ninja <radok.zubets@gmail.com>
2025-05-04 22:52:52 +0300

    some changes

--- src/scripts/hangmansbody.js
+++ src/scripts/hangmansbody.js
@@ -1,30 +1,68 @@
-let WrongAtteps = 0;
-const MaxWrongAtteps = 7;
-const hangmanImage = document.querySelector('#hangman-images');
-const messageDiv = document.querySelector('.header-content > p');
 
 
-function updateHangmanImage() {
-    if (WrongAtteps <= MaxWrongAtteps) {
-        hangmanImage.src = `../../public/img/hangmansbody.png/step${WrongAtteps - 1}.jpg`;
+
+export function hangmanImage() {
+    let WrongAtteps = 0;
+
+    const MaxWrongAtteps = 7;
+
+    const hangmanImage = document.querySelector('#hangman-images');
+    
+    if(WrongAtteps === 0) {
+        ocument.querySelector('#hangman-images');
     }
 }
 
 
-export function addWrongAtteps() {
-    WrongAtteps++;
-    updateHangmanImage(); 
-    if (WrongAtteps === MaxWrongAtteps) {
-        hangmanImage.src = `../../public/img/hangmansbody.png/step${WrongAtteps - 1}.jpg`; 
-        messageDiv.textContent = "You Lose!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function updateHangmanImage() {
+//     if (WrongAtteps <= MaxWrongAtteps) {
+//         hangmanImage.src = `../../public/img/hangmansbody.png/step${WrongAtteps - 1}.jpg`;
+//     }
+// }
+
+
+// export function addWrongAtteps() {
+//     WrongAtteps++;
+//     updateHangmanImage(); 
+//     if (WrongAtteps === MaxWrongAtteps) {
+//         hangmanImage.src = `../../public/img/hangmansbody.png/step${WrongAtteps - 1}.jpg`; 
+//         messageDiv.textContent = "You Lose!";
    
-    }
-}
+//     }
+// }
 
 
-    if (!qustedWord.includes(selectedLetter)) {
-        addWrongAtteps(); 
-    } else {
+//     if (!qustedWord.includes(selectedLetter)) {
+//         addWrongAtteps(); 
+//     } else {
     
-    }
+//     }
     
+
+
+
+
+
+
